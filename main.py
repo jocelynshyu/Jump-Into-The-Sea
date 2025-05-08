4ad3244e3a4de4296aa1b075625c9eee5d2486a0
flyxiang <dinosaur860504@gmail.com>
2025-05-08 13:43:41 +0800

    嘗試修正view會timeout的問題, 整理程式碼

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ logging.basicConfig(handlers=[handler, console_handler], level=logging.INFO, for
 csv_file_path = "./message.csv"
 string_list = load_messages_from_csv(csv_file_path)
 channel_id = 0000000000000000
+message_id = 0
+# client是跟discord連接，intents是要求機器人的權限
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
 
 def get_sea_string():
     random.shuffle(string_list)  # 隨機打亂字串陣列
@@ -53,25 +58,18 @@ class JobButtonHandler(discord.ui.Button):
         new_view.add_item(SeaButtonHandler(label="海港", style=discord.ButtonStyle.success))
         await interaction.response.send_message("> 去跳海吧", view=new_view, ephemeral=True)
 
-# client是跟discord連接，intents是要求機器人的權限
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents = intents)
-
 # 調用event函式庫
 @client.event
 # 當機器人完成啟動
 async def on_ready():
     print(f"目前登入身份 --> {client.user}")
-    view = discord.ui.View()
-    view.clear_items()
-    view.add_item(JobButtonHandler(label="工作", style=discord.ButtonStyle.primary))
+    
     channel = client.get_channel(channel_id)  # Fetch the channel using its ID
     if channel:
         async for msg in channel.history(limit=None):
             await msg.delete()
-        await channel.send(file=discord.File("CountryState_Yiguo.png"), view=view)
-        print("Message sent to channel!")
+        
+        await send_view()
     else:
         print("Channel not found!")
 
@@ -79,33 +77,49 @@ async def on_ready():
 async def on_resumed():
     await on_ready()
 
-# @client.event
-# 當頻道有新訊息
-# async def on_message(message):
-#     # 排除機器人本身的訊息，避免無限循環
-#     if message.author == client.user:
-#         return
-    
-#     if message.channel.id != channel_id:
-#         return
-
-#     # 新訊息包含Hello，回覆Hello, world!
-#     if message.content == "Hello":
-#         await send_view(message.channel)
-
 @client.event
 async def on_error(event, *args, **kwargs):
     print(f"發生錯誤: {event} {args} {kwargs}")
 
-async def send_view(channel):
-    # 創建一個 View，並設置 30 秒超時
-    view = discord.ui.View(timeout = 30)
-    # 添加一個 Button 到 View 中 
+async def edit_resend_view():
+    global message_id  # 明確宣告使用全域變數
+    channel = client.get_channel(channel_id)
 
-    # Replace the existing button with the custom ButtonHandler
-    view.clear_items()
-    view.add_item(JobButtonHandler(label="工作", style=discord.ButtonStyle.primary))
-    await channel.send(file=discord.File("CountryState_Yiguo.png"), view=view)
+    if channel:
+        view = discord.ui.View()
+        view.clear_items()
+        view.add_item(JobButtonHandler(label="工作", style=discord.ButtonStyle.primary))
+
+        async def on_timeout():
+            print("View 已超時，重新生成按鈕。")
+            await edit_resend_view()
+        
+        view.on_timeout = on_timeout
+        sent_message = await channel.fetch_message(message_id)
+        new_message = await sent_message.edit(view=view)
+        message_id = new_message.id
+        print("Message sent to channel!")
+
+async def send_view():
+    global message_id  # 明確宣告使用全域變數
+    channel = client.get_channel(channel_id)
+
+    if channel:
+        view = discord.ui.View()
+        view.clear_items()
+        view.add_item(JobButtonHandler(label="工作", style=discord.ButtonStyle.primary))
+
+        async def on_timeout():
+            if channel:
+                print("View 已超時，重新生成按鈕。")
+                await edit_resend_view()
+        
+        view.on_timeout = on_timeout
+        sent_message = await channel.send(file=discord.File("CountryState.png"), view=view)
+        message_id = sent_message.id
+        print("Message sent to channel!")
+    else:
+        print("Channel not found!")
 
 
 client.run("你的Discord機器人token",log_handler=handler,log_level=logging.INFO)
